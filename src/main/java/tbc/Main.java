f75c1dcb56f95641ac313c223bb1e451eae9702d
Fubuki <talakdavid@gmail.com>
2026-06-23 20:13:05 +0400

    added someIntegerFunction

--- src/main/java/tbc/Main.java
+++ src/main/java/tbc/Main.java
@@ -3,9 +3,14 @@ package tbc;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi");
+        int number = someIntegerFunction();
     }
 
     private  static  void someNewFunction(){
         System.out.println("Hi but new");
+    }
+
+    private static int someIntegerFunction(){
+        return 1;
     }
 }
