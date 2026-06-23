062fe9bd06f0abbb7791f18c9ef24acf45ce8168
Fubuki <talakdavid@gmail.com>
2026-06-23 19:46:58 +0400

    Added someNewFunction

--- src/main/java/tbc/Main.java
+++ src/main/java/tbc/Main.java
@@ -3,5 +3,10 @@ package tbc;
 public class Main {
     public static void main(String[] args) {
         IO.println(String.format("Hello and welcome!"));
+        String result = someNewFunction("MESSAGE");
+    }
+
+    private static String someNewFunction(String message){
+        return  message;
     }
 }
